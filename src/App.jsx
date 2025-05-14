ef82b913ac3622f7b1f6aa8fa21fac7cdc1bc24d
bala-077 <hariharan101311@gmail.com>
2025-05-14 15:36:34 +0530

    make the change on the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar'
-import Gtech from './pages/Gtech'
-import EventManagement from './pages/EventManagement'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Mainlayout from './layout/Mainlayout';
+import Gtech from './pages/Gtech';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Example routes, replace with your actual components */}
-        {/* <Route path="/aboutus" element={<AboutUs />} /> */}
-        <Route path="/gtech" element={<Gtech />} />
-        <Route path="/event-management" element={<EventManagement />} />
-        {/* <Route path="/fmcg" element={<FMCG />} /> */}
-        {/* <Route path="/gallery" element={<Gallery />} /> */}
-        {/* <Route path="/contact" element={<ContactUs />} /> */}
+        <Route path="/" element={<Mainlayout />}>
+          <Route path="gtech" element={<Gtech />} />
+          {/* <Route path="aboutus" element={<AboutUs />} /> */}
+          {/* <Route path="event-management" element={<EventManagement />} /> */}
+          {/* <Route path="fmcg" element={<FMCG />} /> */}
+          {/* <Route path="gallery" element={<Gallery />} /> */}
+          {/* <Route path="contact" element={<ContactUs />} /> */}
+        </Route>
       </Routes>
     </Router>
   );
